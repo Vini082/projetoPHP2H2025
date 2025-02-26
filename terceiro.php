a948cd947d74dd31e4415527bbeaf51e18fa0a73
Vini082 <viniciussantos082@gmail.com>
2025-02-25 22:14:39 -0300

    projeto php

---
+++ terceiro.php
@@ -0,0 +1,15 @@
+<?php
+	$data = "25 de fevereiro de 2025";
+	$salario = 850.00;
+	$cargo = "Estagiario";
+	$idade = 18;
+	$resultado = true;
+
+	if($resultado){
+		echo "Verdadeiro";
+		echo "<br>";
+		echo "Seu salario é " . $salario;
+	}
+
+
+?>
