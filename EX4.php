a78366243942e7165d02f7979b3272ba5f0972f2
Vini082 <viniciussantos082@gmail.com>
2025-03-31 20:16:27 -0300

    salvar

--- EX4.php
+++ EX4.php
@@ -1,15 +1,27 @@
 <?php 
-$n1 = 
-$n2 =
-$n3 = 
-$troca = 
+$n1 =9;
+$n2 =7;
+$n3 =2;
 
-if ($n1>$n2 && $n2>$n3){
+if ($n1>$n2 && $n1>$n3){
+	if($n2>$n3){
 	Echo ("Os numeros em ordem é: " . $n1 ." ". $n2 ." ". $n3);
+}else{
+	Echo ("Os numeros em ordem é: " . $n1 ." ". $n3 ." ". $n2);
 }
-
-
-
-
-
- ?>
+}
+if ($n2>$n1 && $n2>$n3){
+	if($n1>$n3){
+	Echo ("Os numeros em ordem é: " . $n2 ." ". $n1 ." ". $n3);
+}else{
+	Echo ("Os numeros em ordem é: " . $n2 ." ". $n3 ." ". $n1);
+}
+}
+if ($n3>$n1 && $n3>$n2){
+	if($n2>$n1){
+	Echo ("Os numeros em ordem é: " . $n3 ." ". $n2 ." ". $n1);
+}else{
+	Echo ("Os numeros em ordem é: " . $n3 ." ". $n1 ." ". $n2);
+}
+}
+?>
---
+++ EX5.php
@@ -0,0 +1,44 @@
+<?php 
+
+$maioraltura=0;
+$menoraltura=0;
+$somaalturamulheres=0;
+$qtdmulher=0;
+$idadehomemvelho=0;
+$idademulhernova=0;
+
+$p1_altura=1.80;
+$p1_idade=25;
+$p1_sexo="M";
+$p2_altura=1.65;
+$p2_idade=20;
+$p2_sexo="F";
+
+if($p1_altura>$p2_altura){
+$maioraltura=$p1_altura;
+}else{
+$maioraltura=$p2_altura;
+}
+
+if($p1_altura<$p2_altura){
+$menoraltura=$p1_altura;
+}else{
+$menoraltura=$p2_altura;
+}
+
+if()
+$somaalturamulheres = 
+
+// for($i=0;$i<5;$i++){
+// echo("Altura é:".  <br>");
+// echo("Idade é: <br>");
+// echo("O sexo é: <br>");
+
+
+// }
+
+
+
+
+
+ ?>
