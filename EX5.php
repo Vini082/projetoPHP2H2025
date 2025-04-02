4269bcc45db969d64ee253570c22492ec5d3cd91
Vini082 <viniciussantos082@gmail.com>
2025-04-01 22:14:31 -0300

    salvar

--- EX5.php
+++ EX5.php
@@ -3,16 +3,20 @@
 $maioraltura=0;
 $menoraltura=0;
 $somaalturamulheres=0;
-$qtdmulher=0;
+$mediaalturamulher=0;
+$qtdmulher=2;
 $idadehomemvelho=0;
 $idademulhernova=0;
 
 $p1_altura=1.80;
 $p1_idade=25;
-$p1_sexo="M";
-$p2_altura=1.65;
+$p1_sexo="F";
+$p2_altura=1.60;
 $p2_idade=20;
 $p2_sexo="F";
+$cont=0;
+
+//qual a maior e menor altura
 
 if($p1_altura>$p2_altura){
 $maioraltura=$p1_altura;
@@ -26,8 +30,29 @@ $menoraltura=$p1_altura;
 $menoraltura=$p2_altura;
 }
 
-if()
-$somaalturamulheres = 
+echo("A maior altura é: " . $maioraltura . "</br>");
+echo("A menor altura é: " . $menoraltura . "</br>");
+
+//média altura mulheres 
+while($qtdmulher != $cont){
+if($p1_sexo=="F"){
+$somaalturamulheres = $somaalturamulheres + $p1_altura;
+$p1_sexo="";
+$cont++;
+}else if($p2_sexo=="F"){
+$somaalturamulheres = $somaalturamulheres + $p2_altura;
+$p2_sexo="";
+$cont++;
+}
+}
+
+$mediaalturamulher = $somaalturamulheres/$cont;
+echo("A média da altura das mulheres é : " . $mediaalturamulher);
+
+
+//quem é o mais velho e o mais novo
+
+
 
 // for($i=0;$i<5;$i++){
 // echo("Altura é:".  <br>");
